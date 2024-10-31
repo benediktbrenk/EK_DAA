a7f9a2651d1900990a193d40a00b3a2418f6a981
benediktbrenk <benedikt.brenk@outlook.de>
2024-10-31 13:20:35 +0100

    use dom-manipulation and map-function to produce cards in about.php from array

---
+++ data/learnings.js
@@ -0,0 +1,27 @@
+const learnings = [
+	{
+		title: "PHP Classes and Functions",
+		description:
+			"Learn to structure code using classes and functions in PHP, making it more modular and reusable.",
+	},
+	{
+		title: "Form Data Processing in PHP",
+		description:
+			"Able to capture and process form data using PHP, handling user input on the server side.",
+	},
+	{
+		title: "Bootstrap Layouts and Templates",
+		description:
+			"Use Bootstrap’s grid system and templates to create a visually appealing, responsive layout.",
+	},
+	{
+		title: "JavaScript DOM Manipulation",
+		description:
+			"Learn JavaScript DOM manipulation to dynamically update and interact with HTML elements.",
+	},
+	{
+		title: "Version Control with Git",
+		description:
+			"Use Git for version control to track changes, collaborate, and manage code versions.",
+	},
+];
